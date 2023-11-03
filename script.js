4f5772d829156d7cbf448a133a9d61a981fc0c8e
AsadbekXayitov <hackeruza7@gmail.com>
2023-11-03 23:22:03 +0500

    yangi

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let loader = document.querySelector(".loader");
 let wrap = document.querySelector(".wrap");
+let nav = document.querySelector("nav");
 
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         wrap.style.display = "none";
     }, 1000)
 })
+if (nav) {
+    
+} else {
+    
+}
